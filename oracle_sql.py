f9fd138d8c6ef391ccdd622ec4664ae8d6656d7c
Owen Campbell <owen.campbell@empiria.co.uk>
2015-11-12 18:05:48 +0000

    Initial commit

---
+++ oracle_sql.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+import os
+import sys
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser(
+    description="Pass an sql script to Oracle")
+
+parser.add_argument(
+    '-d',
+    '--directory',
+    type=str,
+    dest='directory',
+    help='Path to directory containing sql script')
+
+parser.add_argument(
+    '-f',
+    '--file',
+    type=str,
+    dest='file_name',
+    help='Full file name of sql script')
+
+parser.add_argument(
+    '-u',
+    '--user',
+    type=str,
+    dest='user',
+    help='Oracle user name')
+
+parser.add_argument(
+    '-p',
+    '--password',
+    type=str,
+    dest='password',
+    help='Oracle password')
+
+parser.add_argument(
+    '-s',
+    '--sid',
+    type=str,
+    dest='sid',
+    help='Oracle SID')
+
+try:
+    args = parser.parse_args()
+    file_path = os.path.join(args.directory, args.file_name)
+except:
+    parser.print_help()
+    sys.exit(0)
+
+file = open(file_path, 'r')
+connection = args.user + '/' + args.password + '@' + args.sid
+
+os.chdir(args.directory)
+
+subprocess.run(
+    ['sqlplus', '-S', '-L', connection], stdin=file)
+print('Executed %s against %s' % (args.file_name, args.sid))
