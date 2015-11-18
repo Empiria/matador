a5331d5d99739dbef168acb0c958ee32c4edae8d
Owen Campbell <owen.campbell@empiria.co.uk>
2015-11-18 13:58:57 +0000

    Create stubs for DeployTicket class

--- matador/core/commands/__init__.py
+++ matador/core/commands/__init__.py
@@ -1,7 +1,9 @@
 from .hello import Hello
 from .run_sql_script import RunSqlScript
+from .deploy_ticket import DeployTicket
 
 commands = {
     'hello': Hello,
-    'run-sql-script': RunSqlScript
+    'run-sql-script': RunSqlScript,
+    'deploy-ticket': DeployTicket
 }
---
+++ matador/core/commands/deploy_ticket.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from .command import Command
+
+
+class DeployTicket(Command):
+
+    def _execute(self):
+        pass
