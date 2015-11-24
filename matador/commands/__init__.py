65dd79ac40ce978a5cee4a3ddd0131601807de28
Owen Campbell <owen.campbell@empiria.co.uk>
2015-11-24 14:50:38 +0000

    Create stubs and commands dictionary entries for package commands

--- matador/commands/__init__.py
+++ matador/commands/__init__.py
@@ -1,8 +1,11 @@
 from .run_sql_script import RunSqlScript
 from .deploy_ticket import DeployTicket, RemoveTicket
+from .deploy-package import DeployPackage, RemovePackage
 
 commands = {
     'run-sql-script': RunSqlScript,
     'deploy-ticket': DeployTicket,
-    'remove-ticket': RemoveTicket
+    'remove-ticket': RemoveTicket,
+    'deploy-package': DeployPackage,
+    'remove-package': RemovePackage,
 }
