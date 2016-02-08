2882b03e054f070174b53b750b932288c7df5f7f
Owen Campbell <owen.campbell@empiria.co.uk>
2016-02-08 13:28:41 +0000

    Update version to 1.5.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except LookupError:
 
 setup(
     name='Matador',
-    version='1.5.0',
+    version='1.5.1',
     author='Owen Campbell',
     author_email='owen.campbell@empiria.co.uk',
     entry_points={
