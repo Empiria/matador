3fdeb3626c9d9b267cbbe70f95a3e4a54ae1772c
Owen Campbell <owen.campbell@empiria.co.uk>
2016-05-04 14:31:56 +0100

    Create 2.0.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except LookupError:
 
 setup(
     name='Matador',
-    version='2.0.0',
+    version='2.0.1',
     author='Owen Campbell',
     author_email='owen.campbell@empiria.co.uk',
     entry_points={
