f01776ead14ca6e01a5d445d463d8f6d517ececd
Owen Campbell <owen.campbell@egonzehnder.com>
2017-06-12 11:11:28 +0100

    Add DeployReportFile command

--- matador/commands/deployment/deploy_report.py
+++ matador/commands/deployment/deploy_report.py
@@ -7,7 +7,27 @@ import shutil
 from openpyxl import load_workbook
 
 
-def _fetch_report(repo, report_path, commit_ref, target_folder):
+def _fetch_report_file(repo, report_path, commit_ref, target_folder):
+    report = Path(repo.path, report_path)
+    target_report = Path(target_folder, report.name)
+    version, commit_timestamp, author = git.keyword_values(repo, commit_ref)
+
+    git.checkout(repo, commit_ref)
+
+    with report.open('r') as f:
+        original_text = f.read()
+        f.close()
+
+    new_text = git.substitute_keywords(original_text, repo, commit_ref)
+
+    with target_report.open('w') as f:
+        f.write(new_text)
+        f.close()
+
+    return target_report
+
+
+def _fetch_excelerator_report(repo, report_path, commit_ref, target_folder):
     report = Path(repo.path, report_path)
     target_report = Path(target_folder, report.name)
     version, commit_timestamp, author = git.keyword_values(repo, commit_ref)
@@ -35,7 +55,28 @@ class DeployExceleratorReport(DeploymentCommand):
             str(report_name) + '.xlsx')
 
         commit = self.args[1]
-        report = _fetch_report(
+        report = _fetch_excelerator_report(
+            Session.matador_repo, report_path, commit,
+            Session.deployment_folder)
+        target_folder = (
+            '//' +
+            Session.environment['abwServer'] + '/' +
+            Session.environment['customisedReports'])
+
+        shutil.copy(str(report), target_folder)
+
+
+class DeployReportFile(DeploymentCommand):
+
+    def _execute(self):
+        report_name = Path(self.args[0])
+        report_file_name = Path(self.args[1])
+        repo_folder = Session.matador_repository_folder
+        report_path = Path(
+            repo_folder, 'src', 'reports', report_name, report_file_name)
+
+        commit = self.args[2]
+        report = _fetch_report_file(
             Session.matador_repo, report_path, commit,
             Session.deployment_folder)
         target_folder = (
