c80a54222470f63c52f88beca3c6b63617aed5e9
Owen Campbell <owen.campbell@empiria.co.uk>
2017-10-03 09:45:26 +0100

    Bump version: 4.0.1 → 4.0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except LookupError:
 
 setup(
     name='Matador',
-    version='4.0.1',
+    version='4.0.2',
     author='Empiria Ltd',
     author_email='info@empiria.co.uk',
     entry_points={
